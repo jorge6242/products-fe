f535b5eefc97556c8ad930d1ea034fd92d9f8f92
Unknown <jorge6242@gmail.com>
2019-05-24 05:49:27 -0400

    Prefix for endpoints

--- src/Api/Product.js
+++ src/Api/Product.js
@@ -1,24 +1,25 @@
 import AXIOS from '../Config/Axios';
+import Prefix from '../Config/ApiPrefix';
 
 const Product = {
     getAll() {
-        return AXIOS.get('/product');
+        return AXIOS.get(`${Prefix.api}/product`);
     },
     create(data) {
-        return AXIOS.post(`/product`, {
+        return AXIOS.post(`${Prefix.api}/product`, {
             ...data
         });
     },
     get(id) {
-        return AXIOS.get(`/product/${id}`);
+        return AXIOS.get(`${Prefix.api}/product/${id}`);
     },
     update(data) {
-        return AXIOS.put(`/product/${data.id}`, {
+        return AXIOS.put(`${Prefix.api}/product/${data.id}`, {
             ...data
         });
     },
     remove(id) {
-        return AXIOS.delete(`/product/${id}`);
+        return AXIOS.delete(`${Prefix.api}/product/${id}`);
     },
 }
 
